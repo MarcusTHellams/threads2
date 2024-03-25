a56aaae3063d5e275d434b4836c158f8d909ed69
mhellams <mhellams@collegeboard.org>
2024-03-25 16:18:06 -0400

    finally got the frontend working correctly with the auth and refresh tokens

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,6 +1,8 @@
-import { useQuery, gql } from '@apollo/client';
+import { gql } from '@apollo/client';
+import { useQuery } from '@tanstack/react-query';
 import { useEffect } from 'react';
 import { api } from './common/api';
+import { apolloClient } from './common/apolloClient';
 
 function App() {
 	// useEffect(() => {
@@ -10,21 +12,33 @@ function App() {
 	// 			password: 'password',
 	// 		})
 	// 		.then(({ data: { accessToken } }) => {
-  //       console.log('accessToken: ', accessToken);
+	//       console.log('accessToken: ', accessToken);
 	// 			localStorage.setItem('accessToken', accessToken)
 	// 		})
 	// 		.catch((e) => {
 	// 			console.log('e: ', e);
 	// 		});
 	// }, []);
-	useQuery(gql`
+
+	const query = gql`
 		query HELLO_WORLD {
 			helloWorld
 		}
-	`);
+	`;
+	const { data } = useQuery({
+		queryKey: ['helloWorld'],
+		async queryFn() {
+			return apolloClient
+				.query<{ helloWorld: string }>({
+					query,
+					fetchPolicy: 'no-cache',
+				})
+				.then(({ data: { helloWorld } }) => helloWorld);
+		},
+	});
 	return (
 		<>
-			<h1>Hello World</h1>
+			<h1>{data}</h1>
 		</>
 	);
 }
--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { ApolloProvider } from '@apollo/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { apolloClient } from './common/apolloClient.ts';
 
+const client = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchInterval: false,
+			refetchOnWindowFocus: false,
+		},
+	},
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ApolloProvider client={apolloClient}>
-      <App />
-    </ApolloProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<ApolloProvider client={apolloClient}>
+			<QueryClientProvider client={client}>
+				<App />
+			</QueryClientProvider>
+		</ApolloProvider>
+	</React.StrictMode>
 );
